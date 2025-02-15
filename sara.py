5f9c8fc9de55e85fb2b52986dbe4e68863c057d5
Buck3ts41 <107511158+Buck3ts41@users.noreply.github.com>
2025-02-15 15:00:35 +0100

    Add files via upload

--- sara.py
+++ sara.py
@@ -2,7 +2,9 @@
 Made By @Buck3ts41
 '''
 
-
+import psutil
+import os
+from colorama import Fore, Style
 import pyMeow as pm
 from offsets import *
 import time
@@ -665,6 +667,57 @@ class CS2Esp:
         if it is not None:
             return it
         return "<unknown>"
+
+
+def clear_console():
+    os.system("cls")
+
+def print_banner():
+    clear_console()
+    print(Fore.CYAN + """
+                                                   
+  .--.--.                                           
+ /  /    '.                                         
+|  :  /`. /              __  ,-.                   
+;  |  |--`             ,' ,'/ /|                   
+|  :  ;_      ,--.--.  '  | |' | ,--.--.          
+ \  \    `.  /       \ |  |   ,'/       \         
+  `----.   \.--.  .-. |'  :  / .--.  .-. |        
+  __ \  \  | \__\/ : . .|  | '   \__\/ : . .       
+ /  /`--'  / ," .--.; ||  , ;  /  /`--'  /       
+'--'.     / /  /  ,.  ||  , ;  /  /  ,.  |       
+  `--'---' ;  :   .'   \---'  ;  :   .'   \      
+           |  ,     .-./      |  ,     .-./      
+            `--`---'           `--`---'          
+
+""" + Style.RESET_ALL, '\n')
+    print(Fore.CYAN + "[*] By Buck3ts41", '\n', Style.RESET_ALL)
+    time.sleep(2)
+    clear_console()
+
+def check_process_running(process_name):
+    for proc in psutil.process_iter(['name']):
+        if process_name.lower() in proc.info['name'].lower():
+            return True
+    return False
+
+def main():
+    print_banner()
+    input(Fore.YELLOW + "---> Press ENTER in game <---" + Style.RESET_ALL)
+
+    if check_process_running('cs2'):
+        clear_console()
+        print('[+] CS2 is running, starting Sara...')
+        esp = CS2Esp()
+        esp.run()
+    else:
+        print(Fore.RED + "[!] Process not found or need to Update Offsets" + Style.RESET_ALL)
+        time.sleep(3)
+        exit(0)
+
+if __name__ == "__main__":
+    main()
+
             
     
                 
